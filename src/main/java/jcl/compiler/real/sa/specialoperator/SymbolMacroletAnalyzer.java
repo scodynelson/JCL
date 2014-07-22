8aeb0c42a41b2fe2d6151f2438b2460e3d804046
codynelson <s.codynelson@gmail.com>
2014-07-22 00:22:55 -0400

    First stab a Symbol-Macrolet. Still way far off when dealing with expansions and crap. Not sure how that's gonna work yet...

--- src/main/java/jcl/compiler/real/sa/specialoperator/SymbolMacroletAnalyzer.java
+++ src/main/java/jcl/compiler/real/sa/specialoperator/SymbolMacroletAnalyzer.java
@@ -3,18 +3,53 @@ package jcl.compiler.real.sa.specialoperator;
 import jcl.LispStruct;
 import jcl.compiler.real.sa.Analyzer;
 import jcl.lists.ListStruct;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import jcl.symbols.SpecialOperator;
+import jcl.symbols.SymbolStruct;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SymbolMacroletAnalyzer implements Analyzer<LispStruct, ListStruct> {
 
 	public static final SymbolMacroletAnalyzer INSTANCE = new SymbolMacroletAnalyzer();
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SymbolMacroletAnalyzer.class);
-
 	@Override
 	public LispStruct analyze(final ListStruct input) {
-		LOGGER.error("; Warning: SYMBOL-MACROLET not supported at this time.");
-		return input;
+
+		final LispStruct second = input.getRest().getFirst();
+		if (!(second instanceof ListStruct)) {
+			throw new RuntimeException("SYMBOL-MACROLET: Parameter list must be of type ListStruct. Got: " + second);
+		}
+
+		final ListStruct parameterList = (ListStruct) second;
+		final List<LispStruct> parameterJavaList = parameterList.getAsJavaList();
+
+		for (final LispStruct currentParameter : parameterJavaList) {
+			if (!(currentParameter instanceof ListStruct)) {
+				throw new RuntimeException("SYMBOL-MACROLET: Parameter must be of type ListStruct. Got: " + second);
+			}
+
+			final ListStruct currentParameterList = (ListStruct) currentParameter;
+			if (currentParameterList.size() != 2) {
+				throw new RuntimeException("SYMBOL-MACROLET: ListStruct parameter must have only 2 elements. Got: " + currentParameter);
+			}
+
+			final LispStruct currentParameterListFirst = currentParameterList.getFirst();
+			if (!(currentParameterListFirst instanceof SymbolStruct)) {
+				throw new RuntimeException("SYMBOL-MACROLET: ListStruct parameter first element value must be of type SymbolStruct. Got: " + currentParameter);
+			}
+		}
+
+		// TODO: Handle declarations that happen before the body!!!
+		final ListStruct symbolMacroletBody = input.getRest().getRest();
+		final ListStruct prognResults = PrognAnalyzer.INSTANCE.analyze(symbolMacroletBody);
+		final List<LispStruct> javaPrognResults = prognResults.getAsJavaList();
+
+		final List<LispStruct> symbolMacroletResultList = new ArrayList<>();
+		symbolMacroletResultList.add(SpecialOperator.SYMBOL_MACROLET);
+		symbolMacroletResultList.add(second);
+		symbolMacroletResultList.addAll(javaPrognResults);
+
+		return ListStruct.buildProperList(symbolMacroletResultList);
 	}
 }

aef5da4628f3976aece22783d92dd0458131e865
codynelson <s.codynelson@gmail.com>
2015-02-02 01:01:36 -0500

    A bit of cleanup of the Function Analyzer. Adding some TODOs. Gotta think how i'm gonna approach this one since it can return (function <symbol>) or a new LambdaElement. Hmm...

--- src/main/java/jcl/compiler/real/sa/specialoperator/FunctionAnalyzer.java
+++ src/main/java/jcl/compiler/real/sa/specialoperator/FunctionAnalyzer.java
@@ -3,7 +3,6 @@ package jcl.compiler.real.sa.specialoperator;
 import jcl.LispStruct;
 import jcl.compiler.real.environment.Binding;
 import jcl.compiler.real.environment.Environment;
-import jcl.compiler.real.environment.EnvironmentAccessor;
 import jcl.compiler.real.environment.Marker;
 import jcl.compiler.real.sa.AnalysisBuilder;
 import jcl.compiler.real.sa.SemanticAnalyzer;
@@ -46,13 +45,17 @@ public class FunctionAnalyzer implements SpecialOperatorAnalyzer {
 
 		if (second instanceof SymbolStruct) {
 			final SymbolStruct<?> functionSymbol = (SymbolStruct) second;
-			final Environment fnBinding = EnvironmentAccessor.getBindingEnvironment(parentEnvironment, functionSymbol, false);
+			final Environment fnBinding = getBindingEnvironment(parentEnvironment, functionSymbol);
 
 			if (fnBinding.equals(Environment.NULL)) {
+				// TODO: we should think about what's actually happening here...
 				lexicalSymbolStructAnalyzer.analyzeSymbol(functionSymbol, analysisBuilder);
 				return input;
 			} else {
 				final Optional<Binding> binding = fnBinding.getBinding(functionSymbol);
+
+				// TODO: Refactor this a bit. This should always be true at this point. BUT, we DO want to make sure we're
+				// TODO:    not searching the NULL environment for the binding. Hmm...
 				if (binding.isPresent()) {
 					final Binding bindingValue = binding.get();
 					final SymbolStruct<?> functionBindingName = bindingValue.getSymbolStruct();
@@ -90,4 +93,26 @@ public class FunctionAnalyzer implements SpecialOperatorAnalyzer {
 
 		throw new ProgramErrorException("FUNCTION: First element of List argument must be the Symbol LAMBDA. Got: " + functionListFirst);
 	}
+
+	private static Environment getBindingEnvironment(final Environment environment, final SymbolStruct<?> variable) {
+
+		Environment currentEnvironment = environment;
+
+		while (!currentEnvironment.equals(Environment.NULL)) {
+
+			final Marker marker = currentEnvironment.getMarker();
+			if (Marker.FUNCTION_MARKERS.contains(marker)) {
+
+				final boolean hasBinding = currentEnvironment.hasBinding(variable);
+				if (hasBinding) {
+					break;
+				}
+			}
+
+			currentEnvironment = currentEnvironment.getParent();
+		}
+
+		return currentEnvironment;
+	}
+
 }

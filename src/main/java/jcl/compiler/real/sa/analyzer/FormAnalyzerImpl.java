12181668743551e0a33e790771ab99ef5ca8d755
codynelson <s.codynelson@gmail.com>
2015-03-15 13:54:04 -0400

    Getting rid of one time use analyzers and consolidating them back into the FormAnalyzer

--- src/main/java/jcl/compiler/real/sa/analyzer/FormAnalyzerImpl.java
+++ src/main/java/jcl/compiler/real/sa/analyzer/FormAnalyzerImpl.java
@@ -4,12 +4,32 @@
 
 package jcl.compiler.real.sa.analyzer;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
 import jcl.LispStruct;
 import jcl.compiler.real.environment.Environment;
+import jcl.compiler.real.environment.Environments;
+import jcl.compiler.real.environment.binding.lambdalist.KeyBinding;
+import jcl.compiler.real.environment.binding.lambdalist.OptionalBinding;
+import jcl.compiler.real.environment.binding.lambdalist.OrdinaryLambdaListBindings;
+import jcl.compiler.real.environment.binding.lambdalist.RequiredBinding;
+import jcl.compiler.real.environment.binding.lambdalist.RestBinding;
 import jcl.compiler.real.sa.FormAnalyzer;
 import jcl.compiler.real.sa.analyzer.expander.NewMacroExpand;
 import jcl.compiler.real.sa.analyzer.expander.NewMacroExpandReturn;
+import jcl.compiler.real.struct.functioncall.FunctionCallStruct;
+import jcl.compiler.real.struct.functioncall.LambdaFunctionCallStruct;
+import jcl.compiler.real.struct.specialoperator.lambda.LambdaStruct;
+import jcl.conditions.exceptions.ProgramErrorException;
+import jcl.functions.FunctionStruct;
 import jcl.lists.ConsStruct;
+import jcl.lists.ListStruct;
+import jcl.symbols.KeywordSymbolStruct;
+import jcl.symbols.SpecialOperator;
 import jcl.symbols.SymbolStruct;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -28,7 +48,7 @@ public class FormAnalyzerImpl implements FormAnalyzer {
 	private SymbolAnalyzer symbolAnalyzer;
 
 	@Autowired
-	private ConsAnalyzer consAnalyzer;
+	private LambdaExpander lambdaExpander;
 
 	@Override
 	public LispStruct analyze(final LispStruct input, final Environment environment) {
@@ -39,9 +59,143 @@ public class FormAnalyzerImpl implements FormAnalyzer {
 		if (expandedForm instanceof SymbolStruct) {
 			return symbolAnalyzer.analyze((SymbolStruct<?>) expandedForm, environment);
 		} else if (expandedForm instanceof ConsStruct) {
-			return consAnalyzer.analyze((ConsStruct) expandedForm, environment);
+			return analyzeCons((ConsStruct) expandedForm, environment);
 		} else {
 			return expandedForm;
+		}
+	}
+
+	private LispStruct analyzeCons(final ConsStruct input, final Environment environment) {
+
+		final LispStruct first = input.getFirst();
+
+		if (first instanceof SymbolStruct) {
+			return analyzeSymbolFunctionCall(input, environment);
+		} else if (first instanceof ConsStruct) {
+			return analyzeLambdaFunctionCall(input, environment);
+		} else {
+			throw new ProgramErrorException("SA LIST: First element must be of type SymbolStruct or ListStruct. Got: " + first);
+		}
+	}
+
+	private FunctionCallStruct analyzeSymbolFunctionCall(final ListStruct input, final Environment environment) {
+
+		final SymbolStruct<?> functionSymbol = (SymbolStruct<?>) input.getFirst();
+		final List<LispStruct> functionArguments = input.getRest().getAsJavaList();
+
+		final Set<SymbolStruct<?>> undefinedFunctions = environment.getUndefinedFunctions();
+
+		final FunctionStruct function = functionSymbol.getFunction();
+		if (function == null) {
+			final Stack<SymbolStruct<?>> functionNameStack = environment.getFunctionNameStack();
+
+			if (functionNameStack.contains(functionSymbol)) {
+				// Function is undefined, but name exists on the stack to be created
+				undefinedFunctions.remove(functionSymbol);
+			} else {
+				// Add this as a possible undefined function
+				undefinedFunctions.add(functionSymbol);
+			}
+		} else {
+			// Function is defined
+			undefinedFunctions.remove(functionSymbol);
+
+			final String functionName = functionSymbol.getName();
+			final OrdinaryLambdaListBindings lambdaListBindings = function.getLambdaListBindings();
+			validateFunctionArguments(functionName, lambdaListBindings, functionArguments);
+		}
+
+		final List<LispStruct> analyzedFunctionArguments = new ArrayList<>(functionArguments.size());
+
+		for (final LispStruct functionArgument : functionArguments) {
+			final LispStruct analyzedFunctionArgument = analyze(functionArgument, environment);
+			analyzedFunctionArguments.add(analyzedFunctionArgument);
+		}
+
+		final boolean hasFunctionBinding = Environments.hasFunctionBinding(environment, functionSymbol);
+
+		return new FunctionCallStruct(hasFunctionBinding, functionSymbol, analyzedFunctionArguments);
+	}
+
+	private LambdaFunctionCallStruct analyzeLambdaFunctionCall(final ListStruct input, final Environment environment) {
+
+		// ex ((lambda (x) (+ x 1)) 3)
+		final ListStruct functionList = (ListStruct) input.getFirst();
+
+		final LispStruct functionListFirst = functionList.getFirst();
+
+		if (!functionListFirst.equals(SpecialOperator.LAMBDA)) {
+			throw new ProgramErrorException("LIST ANALYZER: First element of a first element ListStruct must be the SpecialOperator 'LAMBDA'. Got: " + functionListFirst);
+		}
+
+		final LambdaStruct lambdaAnalyzed = lambdaExpander.expand(functionList, environment);
+		final OrdinaryLambdaListBindings lambdaListBindings = lambdaAnalyzed.getLambdaListBindings();
+
+		final List<LispStruct> functionArguments = input.getRest().getAsJavaList();
+
+		validateFunctionArguments("Anonymous Lambda", lambdaListBindings, functionArguments);
+
+		final List<LispStruct> analyzedFunctionArguments = new ArrayList<>(functionArguments.size());
+
+		for (final LispStruct functionArgument : functionArguments) {
+			final LispStruct analyzedFunctionArgument = analyze(functionArgument, environment);
+			analyzedFunctionArguments.add(analyzedFunctionArgument);
+		}
+
+		return new LambdaFunctionCallStruct(lambdaAnalyzed, analyzedFunctionArguments);
+	}
+
+	private static void validateFunctionArguments(final String functionName, final OrdinaryLambdaListBindings lambdaListBindings,
+	                                                final List<LispStruct> functionArguments) {
+
+		final Iterator<LispStruct> functionArgumentsIterator = functionArguments.iterator();
+
+		LispStruct nextArgument = null;
+
+		final List<RequiredBinding> requiredBindings = lambdaListBindings.getRequiredBindings();
+		for (final RequiredBinding ignored : requiredBindings) {
+			if (!functionArgumentsIterator.hasNext()) {
+				throw new ProgramErrorException("LIST ANALYZER: Too few arguments in call to '" + functionName + "'. " + functionArguments.size() + " arguments provided, at least " + requiredBindings.size() + " required.");
+			}
+			nextArgument = functionArgumentsIterator.next();
+		}
+
+		final List<OptionalBinding> optionalBindings = lambdaListBindings.getOptionalBindings();
+		for (final OptionalBinding ignored : optionalBindings) {
+			if (!functionArgumentsIterator.hasNext()) {
+				break;
+			}
+			nextArgument = functionArgumentsIterator.next();
+		}
+
+		final List<KeyBinding> keyBindings = lambdaListBindings.getKeyBindings();
+		final List<KeywordSymbolStruct> keys = new ArrayList<>(keyBindings.size());
+		for (final KeyBinding keyBinding : keyBindings) {
+			final KeywordSymbolStruct key = keyBinding.getKeyName();
+			keys.add(key);
+		}
+
+		final RestBinding restBinding = lambdaListBindings.getRestBinding();
+
+		while (functionArgumentsIterator.hasNext()) {
+			if (!keyBindings.isEmpty()) {
+				if (nextArgument instanceof KeywordSymbolStruct) {
+					final KeywordSymbolStruct argumentKey = (KeywordSymbolStruct) nextArgument;
+					if (keys.contains(argumentKey)) {
+						// Consume the next argument
+						functionArgumentsIterator.next();
+						nextArgument = functionArgumentsIterator.next();
+					} else {
+						throw new ProgramErrorException("LIST ANALYZER: Keyword argument not found in '" + functionName + "' function definition: " + argumentKey);
+					}
+				} else {
+					throw new ProgramErrorException("LIST ANALYZER: Expected Keyword argument for call to '" + functionName + " was: " + nextArgument);
+				}
+			} else if (restBinding != null) {
+				functionArgumentsIterator.next();
+			} else {
+				throw new ProgramErrorException("LIST ANALYZER: Too many arguments in call to '" + functionName + "'. " + functionArguments.size() + " arguments provided, at most " + requiredBindings.size() + " accepted.");
+			}
 		}
 	}
 

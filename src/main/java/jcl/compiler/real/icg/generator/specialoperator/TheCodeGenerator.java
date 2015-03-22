0167ca83b463295a653a6007fd88117f4734eed1
codynelson <s.codynelson@gmail.com>
2015-03-22 19:06:58 -0400

    Adding implementation for The code generator.

--- src/main/java/jcl/compiler/real/icg/generator/specialoperator/TheCodeGenerator.java
+++ src/main/java/jcl/compiler/real/icg/generator/specialoperator/TheCodeGenerator.java
@@ -1,15 +1,24 @@
 package jcl.compiler.real.icg.generator.specialoperator;
 
+import jcl.LispStruct;
 import jcl.compiler.real.icg.JavaClassBuilder;
 import jcl.compiler.real.icg.generator.CodeGenerator;
-import jcl.lists.ListStruct;
+import jcl.compiler.real.icg.generator.FormGenerator;
+import jcl.compiler.real.struct.specialoperator.TheStruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TheCodeGenerator implements CodeGenerator<ListStruct> {
+public class TheCodeGenerator implements CodeGenerator<TheStruct> {
+
+	@Autowired
+	private FormGenerator formGenerator;
 
 	@Override
-	public void generate(final ListStruct input, final JavaClassBuilder classBuilder) {
-		//TODO unimplemented 'the'
+	public void generate(final TheStruct input, final JavaClassBuilder classBuilder) {
+		// TODO: do we want to add the logic here to verify the type information of the generated form???
+
+		final LispStruct form = input.getForm();
+		formGenerator.generate(form, classBuilder);
 	}
 }

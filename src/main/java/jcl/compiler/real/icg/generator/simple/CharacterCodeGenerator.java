9c26e2941c4520183191092e26afc2c5dec5f817
codynelson <s.codynelson@gmail.com>
2015-04-06 11:16:00 -0400

    Updating constructor init definition in CharacterStruct CG.

--- src/main/java/jcl/compiler/real/icg/generator/simple/CharacterCodeGenerator.java
+++ src/main/java/jcl/compiler/real/icg/generator/simple/CharacterCodeGenerator.java
@@ -5,6 +5,7 @@ import jcl.compiler.real.icg.ClassDef;
 import jcl.compiler.real.icg.JavaClassBuilder;
 import jcl.compiler.real.icg.generator.CodeGenerator;
 import jcl.compiler.real.icg.generator.GenerationConstants;
+import jcl.compiler.real.icg.generator.GeneratorUtils;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
@@ -15,7 +16,7 @@ public class CharacterCodeGenerator implements CodeGenerator<CharacterStruct> {
 
 	private static final String CHARACTER_STRUCT_NAME = Type.getInternalName(CharacterStruct.class);
 
-	private static final String CHARACTER_STRUCT_INIT_DESC = "(I)V";
+	private static final String CHARACTER_STRUCT_INIT_DESC = GeneratorUtils.getConstructorDescription(CharacterStruct.class, int.class);
 
 	@Override
 	public void generate(final CharacterStruct input, final JavaClassBuilder classBuilder) {

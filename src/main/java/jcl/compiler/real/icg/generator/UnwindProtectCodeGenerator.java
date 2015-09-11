bb395b181c6139463440e64729e8291d4ddbffe1
codynelson <s.codynelson@gmail.com>
2015-09-11 00:25:10 -0400

    Cleanup and Javadoc additions to UnwindProtect CG.

--- src/main/java/jcl/compiler/real/icg/generator/UnwindProtectCodeGenerator.java
+++ src/main/java/jcl/compiler/real/icg/generator/UnwindProtectCodeGenerator.java
@@ -12,27 +12,60 @@ import org.objectweb.asm.Opcodes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Class to perform 'unwind-protect' special operator code generation.
+ */
 @Component
-class UnwindProtectCodeGenerator extends SpecialOperatorCodeGenerator<UnwindProtectStruct> {
+final class UnwindProtectCodeGenerator extends SpecialOperatorCodeGenerator<UnwindProtectStruct> {
 
+	/**
+	 * {@link IntermediateCodeGenerator} used for generating the {@link UnwindProtectStruct#protectedForm}.
+	 */
 	@Autowired
 	private IntermediateCodeGenerator codeGenerator;
 
+	/**
+	 * {@link PrognCodeGenerator} used for generating the {@link UnwindProtectStruct#cleanupForms}.
+	 */
 	@Autowired
 	private PrognCodeGenerator prognCodeGenerator;
 
+	/**
+	 * Private constructor which passes 'unwindProtect' as the prefix value to be set in it's {@link #methodNamePrefix}
+	 * value.
+	 */
 	private UnwindProtectCodeGenerator() {
 		super("unwindProtect");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Generation method for {@link UnwindProtectStruct} objects. As an example, it will transform {@code
+	 * (unwind-protect 1 2)} into the following Java code:
+	 * <pre>
+	 * {@code
+	 * private LispStruct unwindProtect_1(Closure var1) {*
+	 *      try {
+	 *          BigInteger var2 = new BigInteger("1");
+	 *          return new IntegerStruct(var2);
+	 *      } finally {
+	 *          BigInteger var3 = new BigInteger("2");
+	 *          new IntegerStruct(var3);
+	 *      }
+	 * }
+	 * }
+	 * </pre>
+	 *
+	 * @param input
+	 * 		the {@link UnwindProtectStruct} input value to generate code for
+	 * @param generatorState
+	 * 		stateful object used to hold the current state of the code generation process
+	 */
 	@Override
 	protected void generateSpecialOperator(final UnwindProtectStruct input, final GeneratorState generatorState,
 	                                       final JavaMethodBuilder methodBuilder, final int closureArgStore) {
 
 		final MethodVisitor mv = methodBuilder.getMethodVisitor();
-
-		final LispStruct protectedForm = input.getProtectedForm();
-		final PrognStruct cleanupForms = input.getCleanupForms();
 
 		final Label tryBlockStart = new Label();
 		final Label tryBlockEnd = new Label();
@@ -40,16 +73,26 @@ class UnwindProtectCodeGenerator extends SpecialOperatorCodeGenerator<UnwindProt
 		final Label catchBlockEnd = new Label();
 		mv.visitTryCatchBlock(tryBlockStart, tryBlockEnd, catchBlockStart, null);
 
+		// Start 'try{}'
 		mv.visitLabel(tryBlockStart);
+
+		final LispStruct protectedForm = input.getProtectedForm();
 		codeGenerator.generate(protectedForm, generatorState);
+
 		final int protectedFormStore = methodBuilder.getNextAvailableStore();
 		mv.visitVarInsn(Opcodes.ASTORE, protectedFormStore);
 
+		// End 'try{}'
 		mv.visitLabel(tryBlockEnd);
+
+		// Non-exception 'cleanup forms' (aka. finally{})
+		final PrognStruct cleanupForms = input.getCleanupForms();
 		prognCodeGenerator.generate(cleanupForms, generatorState);
 		mv.visitInsn(Opcodes.POP);
+
 		mv.visitJumpInsn(Opcodes.GOTO, catchBlockEnd);
 
+		// Start 'catch(Throwable t){}'
 		mv.visitLabel(catchBlockStart);
 		final int exceptionStore = methodBuilder.getNextAvailableStore();
 		mv.visitVarInsn(Opcodes.ASTORE, exceptionStore);
@@ -60,6 +103,7 @@ class UnwindProtectCodeGenerator extends SpecialOperatorCodeGenerator<UnwindProt
 		mv.visitVarInsn(Opcodes.ALOAD, exceptionStore);
 		mv.visitInsn(Opcodes.ATHROW);
 
+		// End 'catch(Throwable t){}'
 		mv.visitLabel(catchBlockEnd);
 		mv.visitVarInsn(Opcodes.ALOAD, protectedFormStore);
 

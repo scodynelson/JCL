66e78dd0348cecbbfa1ea98b99844cc705228ea7
codynelson <s.codynelson@gmail.com>
2015-12-23 11:24:28 -0500

    Adding SimpleSymbolStruct to get rid of forced typing of Symbol Structures for non-internal unknown symbol value types.

---
+++ src/main/java/jcl/symbols/SimpleSymbolStruct.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2011-2014 Cody Nelson - All rights reserved.
+ */
+
+package jcl.symbols;
+
+import jcl.LispStruct;
+
+public class SimpleSymbolStruct extends SymbolStruct<LispStruct> {
+
+	public SimpleSymbolStruct(final String name) {
+		super(name);
+	}
+}

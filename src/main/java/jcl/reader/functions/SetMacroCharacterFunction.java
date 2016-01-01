53a14b2b1e51868e51cfa1c61a8d43ed0c2fea18
codynelson <s.codynelson@gmail.com>
2015-12-31 22:07:36 -0500

    Adding reader functions.

---
+++ src/main/java/jcl/reader/functions/SetMacroCharacterFunction.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2011-2014 Cody Nelson - All rights reserved.
+ */
+
+package jcl.reader.functions;
+
+/**
+ * Created by codynelson on 12/31/15.
+ */
+public class SetMacroCharacterFunction {
+}

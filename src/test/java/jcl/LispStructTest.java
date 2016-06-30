acb8a024e258ef0adc83bac18b46d85bce55cc40
codynelson <s.codynelson@gmail.com>
2016-06-29 20:34:49 -0400

    Updating parts of the build.gradle

---
+++ src/test/java/jcl/LispStructTest.java
@@ -0,0 +1,15 @@
+package jcl;
+
+import org.junit.Test;
+
+/**
+ * Created by codynelson on 6/29/16.
+ */
+public class LispStructTest {
+
+	@Test
+	public void testSomething() {
+		new LispStruct() {
+		};
+	}
+}

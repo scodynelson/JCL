5edd21b27236d70c2ec9727813062d77d504cef5
snelson <cody.nelson@sparcedge.com>
2017-04-14 17:33:08 -0400

    Adding List Mapping Functions.

---
+++ jcl-functions/src/main/java/jcl/functions/list/ConsMappingFunction.java
@@ -0,0 +1,89 @@
+package jcl.functions.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jcl.functions.CommonLispBuiltInFunctionStructBase;
+import jcl.lang.FunctionStruct;
+import jcl.lang.LispStruct;
+import jcl.lang.ListStruct;
+import jcl.lang.NILStruct;
+import jcl.lang.factory.LispStructFactory;
+import jcl.lang.function.parameterdsl.Parameters;
+
+abstract class ConsMappingFunction extends CommonLispBuiltInFunctionStructBase {
+
+	static final String LIST_ACCUMULATE = "LIST";
+	static final String NCONC_ACCUMULATE = "NCONC";
+
+	protected ConsMappingFunction(final String documentation, final String functionName,
+	                              final Parameters parameters) {
+		super(documentation, functionName, parameters);
+	}
+
+	LispStruct map1(final FunctionStruct function, final ListStruct originalArglists,
+	                          final String accumulate, final boolean takeCar) {
+		final ListStruct arglists = originalArglists.copyList();
+		final ListStruct retList = LispStructFactory.toProperList(NILStruct.INSTANCE);
+		ListStruct temp = retList;
+
+		// Outer 'DO' vars
+		ListStruct args = NILStruct.INSTANCE;
+
+		while (true) {
+			for (final LispStruct x : arglists) {
+				if (NILStruct.INSTANCE.equals(x)) {
+					if (accumulate != null) {
+						return retList.getCdr();
+					} else {
+						return originalArglists.getCar();
+					}
+				}
+			}
+
+			// Inner 'DO' vars
+			ListStruct l = arglists;
+
+			while (!NILStruct.INSTANCE.equals(l)) {
+
+				// PUSH
+				final LispStruct carToPush;
+				if (takeCar) {
+					carToPush = ((ListStruct) l.getCar()).getCar();
+				} else {
+					carToPush = l.getCar();
+				}
+				args = LispStructFactory.toCons(carToPush, args);
+
+				// SETF
+				final LispStruct cdarL = ((ListStruct) l.getCar()).getCdr();
+				l.setCar(cdarL);
+
+				// Update l Inner 'DO' var
+				l = (ListStruct) l.getCdr();
+			}
+
+			// SETQ
+			final LispStruct res = function.apply(args.nReverse().toArray());
+
+			// CASE
+			switch (accumulate) {
+				case NCONC_ACCUMULATE:
+					final List<ListStruct> lists = new ArrayList<>();
+					for (final LispStruct curr : temp) {
+						lists.add((ListStruct) curr);
+					}
+					final ListStruct nconc = (ListStruct) ListStruct.nConc(lists, res);
+					temp = (ListStruct) nconc.last();
+					break;
+				case LIST_ACCUMULATE:
+					temp.setCdr(LispStructFactory.toProperList(res));
+					temp = (ListStruct) temp.getCdr();
+					break;
+			}
+
+			// Update Args Outer 'DO' var
+			args = NILStruct.INSTANCE;
+		}
+	}
+}
---
+++ jcl-functions/src/main/java/jcl/functions/list/MapcarFunction.java
@@ -0,0 +1,33 @@
+package jcl.functions.list;
+
+import java.util.List;
+
+import jcl.lang.FunctionStruct;
+import jcl.lang.LispStruct;
+import jcl.lang.factory.LispStructFactory;
+import jcl.lang.function.parameterdsl.Arguments;
+import jcl.lang.function.parameterdsl.Parameters;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class MapcarFunction extends ConsMappingFunction {
+
+	private static final String FUNCTION_NAME = "MAPCAR";
+	private static final String FN_ARGUMENT = "FN";
+
+	public MapcarFunction() {
+		super("",
+		      FUNCTION_NAME,
+		      Parameters.forFunction(FUNCTION_NAME)
+		                .requiredParameter(FN_ARGUMENT)
+		                .restParameter()
+		);
+	}
+
+	@Override
+	public LispStruct apply(final Arguments arguments) {
+		final FunctionStruct function = arguments.getRequiredArgument(FN_ARGUMENT, FunctionStruct.class);
+		final List<LispStruct> lists = arguments.getRestArgument();
+		return map1(function, LispStructFactory.toProperList(lists), LIST_ACCUMULATE, true);
+	}
+}

4c4dbe9f8b6f62ad5869a1b942f2ac38563fe16d
cody <s.codynelson@gmail.com>
2022-05-22 20:18:25 -0400

    Reworking build to fix Sonar code coverage reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.github.ben-manes.versions") version "0.42.0"
     id("java")
     id("jacoco")
+    id("org.sonarqube") version "3.3"
 }
 
 // ************************************************
